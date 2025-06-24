1fb6efc8cbc2cbe622060e9d1322c54c9d150d9a
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-06-23 23:10:39 -0300

    teste

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -4,7 +4,8 @@ import "./components/router-outlet/router-outlet.component.js";
 export class AppComponent extends Component {
 
     constructor() {
-        super("../src/app/app.component.html", "../src/app/app.component.css");
+        // super("../src/app/app.component.html", "../src/app/app.component.css");
+        this.shadowRoot.innerHTML = "Olá, Mundo!";
     }
 
     connectedCallback() {

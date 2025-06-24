2717dd68bb77495b42f411b8b211c97d8e74cf2d
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-06-23 23:13:20 -0300

    teste

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,16 +1,25 @@
 import { Component } from "../../core/component.js";
-import "./components/router-outlet/router-outlet.component.js";
+import "/components/router-outlet/router-outlet.component.js";
 
-export class AppComponent extends Component {
+// export class AppComponent extends Component {
 
+//     constructor() {
+//         super("../src/app/app.component.html", "../src/app/app.component.css");
+//     }
+
+//     connectedCallback() {
+//         super.connectedCallback().then(() => {
+//         });
+//     }
+// }
+
+export class AppComponent extends HTMLElement {
     constructor() {
-        // super("../src/app/app.component.html", "../src/app/app.component.css");
-        this.shadowRoot.innerHTML = "Olá, Mundo!";
+        this.attachShadow({ mode: "open" });
     }
 
     connectedCallback() {
-        super.connectedCallback().then(() => {
-        });
+        this.shadowRoot.innerHTML = "Olá, Mundo!";
     }
 }
 
